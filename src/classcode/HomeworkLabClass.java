cb39d780cea346ad603078e9eab70bdfd1a22a97
Fortesa Nikci <fortesanikci@Fortesas-MacBook-Pro.local>
2022-03-05 10:44:17 -0500

    firstLabWork

---
+++ src/classcode/HomeworkLabClass.java
@@ -0,0 +1,15 @@
+package classcode;
+
+public class HomeworkLabClass {
+    public static void main(String[] args) {
+
+
+        int pen = 2 ;
+        String text = "I am printing the nr of pens ";
+        boolean confirm = true ;
+        String space = " ";
+
+        System.out.print("number of pens " + pen);
+
+    }
+}
