696a6b14dfa8377f487eaf3692910deec6e5b648
Fortesa Nikci <fortesanikci@Fortesas-MacBook-Pro.local>
2022-03-06 23:33:59 -0500

    Homework2

---
+++ src/classcode/Homework2.java
@@ -0,0 +1,63 @@
+package classcode;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        byte dayHours = 24;
+        System.out.println( "One Day Has " + dayHours +" Hours" );
+
+        short daysInAYear = 365;
+        System.out.println("One Year Has " + daysInAYear + " Days" );
+
+        int nrOfEmployees = 1200 ;
+        System.out.println("Our Organization Has " + nrOfEmployees + " Employees" );
+
+        long population = 1800000 ;
+        System.out.println("Population Of Kosovo Is " + population );
+
+        float mortgageRate = 3.75f ;
+        char percentage = '%' ;
+        System.out.println("The Mortgage Rate Is " + mortgageRate + percentage);
+
+        double bankBalance = 2100000 ;
+        char dollar = '$' ;
+        String space = " " ;
+        System.out.println("My Bank Account Balance Is " + bankBalance + space + dollar);
+
+        boolean sunTrue = true ;
+        boolean sunFalse = false ;
+        System.out.println("Does The Sun Rise From The South " + sunFalse );
+        System.out.println("Does The Sun Rise From The East " + sunTrue );
+
+        char myFirstInitial = 'K' ;
+        char mySecondInitial = 'N' ;
+        System.out.println("My Name Initials Are " + myFirstInitial + mySecondInitial);
+
+        char c = 'ç' ;
+        String kn = "Kreshnik Nik" + c + "i" ;
+        System.out.println("My Name Is " + kn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
