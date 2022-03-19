17c4a90d1abe219ef78c6fa38b2283264a9e80b7
Fortesa Nikci <fortesanikci@Fortesas-MacBook-Pro.local>
2022-03-18 20:19:12 -0400

    Class

---
+++ src/Class7/excercise.java
@@ -0,0 +1,9 @@
+package Class7;
+
+public class excercise {
+    public static void main(String[] args) {
+        char [] letters = {'l' , 'k' , 'j' , 'h' , 'm' , 'o' , 'p' , 'i'} ;
+        for ( int k=0 ; k <= letters.length - 1 ; k++)
+            System.out.println(letters[k]);
+    }
+}
